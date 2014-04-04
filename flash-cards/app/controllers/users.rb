68562719b9b9bd7d6d54d9e84b7423208f49986e
Apprentice <apprentice@dbc-chicago-009.local>
2014-04-04 17:20:21 -0500

    Added create account functionality

--- flash-cards/app/controllers/users.rb
+++ flash-cards/app/controllers/users.rb
@@ -20,3 +20,13 @@ get '/log_out' do
   session.clear
   redirect '/'
 end
+
+post '/create_user' do
+  @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+  if @user.save
+    session[:user_id] = User.where(password: params[:password]).first.id
+    redirect '/deck'
+  else
+    erb :index
+  end
+end
