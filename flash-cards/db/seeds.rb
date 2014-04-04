0e4a3f05d30df82843203990e425440bbedfaaed
Apprentice <apprentice@dbc-chicago-009.local>
2014-04-04 15:14:37 -0500

    Created databases
    Fixed quotations in csv definitions.  Fixed seeding hash and added
    deck_id to seed hash.

--- flash-cards/db/seeds.rb
+++ flash-cards/db/seeds.rb
@@ -1,14 +1,14 @@
 class DeckImporter
   def self.import(deck_name, filename)
-    new_deck = Deck.create(title: deck_name)
+    new_deck = Deck.create(name: deck_name)
     csv = CSV.new(File.open(filename), :headers => true)
     csv.each do |row|
-      card_hash = {term: nil, definition: nil}
+      card_hash = {term: nil, definition: nil, deck_id: new_deck.id}
       row.each do |field, value|
         if field == "term"
-          card_hash[term: value]
+          card_hash[:term] = value
         else
-          card_hash[definition: value]
+          card_hash[:definition] = value
         end
       end
       new_deck.cards << Card.create(card_hash)
