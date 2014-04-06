d14c0bfbfad8091c31e4bda6fdabe4cb0bf5d5db
Apprentice <apprentice@dbc-chicago-010.local>
2014-04-06 17:50:45 -0500

    Added jquery to hide and show the results area of the play page
    Passing user guess and real answer from jquery to sinatra using JSON

--- flash-cards/public/js/application.js
+++ flash-cards/public/js/application.js
@@ -1,7 +1,19 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+$(document).ready(function () {
+  $('#result-table').hide();
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $("#target").submit(function(event){
+    event.preventDefault();
+    var str = $( '#target' ).serialize();
+    var input = $("input:radio[name=option]:checked").val();
+    var realAnswer = $( '#real-answer').val();
+    var data = {"option": input, "real_answer": realAnswer};
+
+    $.post('/deck/result', data, function(response) {
+      console.log(response["user_guess"]);
+      $('#result').append(response["result"]);
+      $('#real_answer').append(response["real_answer"]);
+      $('#user_guess').append(response["user_guess"]);
+      $('#result-table').show();
+    });
+  });
 });
